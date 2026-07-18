fea7bd7f4a528490b2d4ab04e810baff705763d5
Said-bourhabi <abousaid11araqi@gmail.com>
2026-07-18 21:31:33 +0100

    feat (main): add accordion equipment heading section

---
+++ fit-app/src/components/main/Equipment.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Equipment = () => {
+    return (
+        <section>
+            <div>
+                <h1 className='text-[#F0DE36]'>
+                    THE EQUIPMENT YOU WANT AND AMENITIES YOU NEED
+                </h1>
+                <p className='text-black text-balance'>
+                    Every club features a variety of equipment and spaces designed to support all types of strength, cardio, interval, and recovery workouts.
+                    Along with the amenities and convenience you need to get a great workout and enjoy your time at the gym.
+                </p>
+            </div>
+        </section>
+    );
+};
+
+export default Equipment;
